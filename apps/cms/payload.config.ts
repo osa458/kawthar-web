dd2506f5c2aab8e38580c316af7b178d920abf6f
Osamah Ziad Abdallah <oabdal2@illinois.edu>
2025-10-27 15:53:10 -0500

    fix: Update to Payload v3 with S3 storage

--- apps/cms/payload.config.ts
+++ apps/cms/payload.config.ts
@@ -1,7 +1,8 @@
-import { buildConfig } from 'payload/config'
+import { buildConfig } from 'payload'
 import { webpackBundler } from '@payloadcms/bundler-webpack'
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { slateEditor } from '@payloadcms/richtext-slate'
+import { s3Storage } from '@payloadcms/storage-s3'
 
 import { Users } from './collections/Users'
 
@@ -12,6 +13,22 @@ export default buildConfig({
   },
   editor: slateEditor({}),
   collections: [Users],
+  plugins: [
+    s3Storage({
+      collections: {
+        'users': true,
+      },
+      bucket: process.env.S3_BUCKET || '',
+      config: {
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+        },
+        endpoint: process.env.S3_ENDPOINT,
+        region: 'nyc3',
+      },
+    }),
+  ],
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URL,
